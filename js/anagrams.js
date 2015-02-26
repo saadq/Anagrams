71fc3c21c6fa1bf39ee18802c821704454dff841
Saad Quadri <saadquadri95@gmail.com>
2015-02-26 15:16:46 -0500

    Recreated the game from scratch in a more object-oriented structure

--- js/anagrams.js
+++ js/anagrams.js
@@ -1,68 +1,45 @@
 (function() {
   'use strict';
 
-  // Dictionary object used to manipulate anagrams
-  var dictionary = {};
+  /**
+   * Constructor function for Glossary object
+   */
+  function Glossary(words) {
+    this._words = words;
+    this._length = this._words.length;
+  }
 
-  // List of words that are used for anagram questions
-  dictionary.words = [
-    'adaxial', 'agreeably', 'antinoise', 'asthenia', 'astint', 'babushka', 'bailiffry',
-    'bathtub', 'bestab', 'bestiary', 'bibulous', 'bordage', 'bostonite',
-    'brogue', 'brushoff', 'budlet', 'cathepsin', 'centesimi', 'chaste', 'chicayote',
-    'coastal', 'coppice', 'couple', 'cuapinole', 'cytoplasm', 'daubingly',
-    'dearth', 'deasil', 'drightin', 'drudge', 'ejecta', 'feelable',
-    'fistnote', 'flareback', 'folial', 'fortunate', 'garrulous', 'gemmology',
-    'glaringly', 'gleet', 'globule', 'gluepot', 'googol', 'googul',
-    'humuslike', 'ichnology', 'illiberal', 'issite', 'karyotin', 'kella',
-    'ketol', 'knowingly', 'lysogenic', 'macaque', 'meddle', 'menseful',
-    'mocha', 'mournival', 'musher', 'natty', 'nonactive', 'nonserous',
-    'outcut', 'outspeak', 'overheavy', 'partially', 'pernor', 'picnic',
-    'prickwood', 'pyorrheal', 'redly', 'refine', 'regaler', 'rollick',
-    'sandling', 'sarcastic', 'scypha', 'severely', 'sinkage', 'sissyish',
-    'sogging', 'staling', 'steellike', 'stonelike', 'stoneware', 'tadpolism',
-    'tarditude', 'tazia', 'thymiosis', 'tightener', 'tritical', 'trundler',
-    'undenuded', 'underbank', 'unpaining', 'untraded', 'wayfare', 'woodworm',
-    'woofer', 'zemeism'
-  ];
-
-  // Stores the count of the remaining words
-  dictionary.wordCount = dictionary.words.length;
+  // Reference to the Glossary prototype
+  var glossaryProto = Glossary.prototype;
 
   /**
-   * Update the current count of the remaining words
+   * Returns a random word from the current glossary
    */
-  dictionary.updateWordCount = function() {
-    this.wordCount = this.words.length;
+  glossaryProto.randWord = function() {
+    return this._words[Math.floor(Math.random() * this._length)];
   };
 
   /**
-   * Returns a random lowercase letter
+   * Removes a word from the current glossary
    */
-  dictionary.randLetter = function() {
-    return String.fromCharCode(97 + Math.floor(Math.random() * 26));
+  glossaryProto.removeWord = function(word) {
+    var index = this._words.indexOf(word);
+    this._words.splice(index, 1);
+    this.updateWordCount();
   };
 
   /**
-   * Replaces one letter of a word with a randomly selected letter
+   * Updates _length of the current glossary
    */
-  dictionary.replaceLetter = function(word) {
-    var index = Math.floor(Math.random() * 100 % word.length);
-    var newWord = word.slice(0, index) + word.slice(index + 1);
-    return newWord += this.randLetter();
+  glossaryProto.updateWordCount = function() {
+    this._length = this._words.length;
   };
 
   /**
-   * Returns a random word from dictionary.words
+   * Shuffles the characters of a String
    */
-  dictionary.randWord = function() {
-    return this.words[Math.floor(Math.random() * 100 % this.wordCount)];
-  };
-
-  /**
-   * Randomly shuffles the letters around in a word
-   */
-  dictionary.shuffle = function(word) {
-    var fragments = word.split('');
+  String.prototype.shuffle = function() {
+    var fragments = this.split('');
     for(var i = fragments.length; i > 0;) {
       var random = parseInt(Math.random() * i);
       var temp = fragments[--i];
@@ -73,197 +50,340 @@
   };
 
   /**
-   * Removes a word from dictionary.words
+   * Sorts a String alphabetically
    */
-  dictionary.removeWord = function(word) {
-    var index = dictionary.words.indexOf(word);
-    var removedWord = dictionary.words.splice(index, 1);
+  String.prototype.sort = function() {
+    return this.split('').sort().join('');
   };
 
   /**
-   * Sorts a word into alphabetical order
+   * Replaces one letter of a String with a randomly selected letter
    */
-  dictionary.sort = function(word) {
-    return word.split('').sort().join('');
-  };
-
-  /**
-   * Returns the correct answer for the current word
-   */
-  dictionary.getCorrectChoice = function(word) {
-    return this.shuffle(word);
-  };
-
-  /**
-   * Returns an incorrect answer for the current word
-   */
-  dictionary.getIncorrectChoice = function(word) {
-    word = this.replaceLetter(word);
-    return this.shuffle(word);
+  String.prototype.replaceLetter = function() {
+    var index = Math.floor(Math.random() * 100 % this.length);
+    var newWord = this.slice(0, index) + this.slice(index + 1);
+    return newWord += randLetter();
   };
 
   /**
    * Checks to see if the given word is a correct answer
    */
-  dictionary.isCorrect = function(word) {
-    return this.sort(word) === this.sort(dictionary.correctButton.innerHTML);
+  String.prototype.isCorrect = function() {
+    return this.sort() === game.correctButton.innerHTML.sort();
   };
 
-  // View object used to change information displayed on the page
-  var view = {};
-
-  // Displays the player's score
-  view.score = document.getElementById('score');
-
-  // Stores the player's current numerical score value
-  view.scoreNum = 0;
-
-  // Stores the current word being used
-  view.currWord = document.getElementById('currentWord');
-
-  // Array of the four button choices
-  view.buttons = document.getElementsByClassName('anagramButton');
-
-  // Variable used to check if the game is over
-  view.gameOver = false;
-
-  // Start the current timer at 5 seconds
-  view.seconds = 500;
-
-  // Used to start a new countdown timer
-  view.timer = null;
+  /**
+   * Returns a random lowercase letter
+   */
+  var randLetter = function() {
+    return String.fromCharCode(97 + Math.floor(Math.random() * 26));
+  };
 
   /**
-   * The event handler for each button
+   * Returns the correct answer for the current word
    */
-  view.makeClickHandler = function(event) {
-    if(event.target === dictionary.correctButton && !view.gameOver) {
-      dictionary.removeWord(view.currWord.innerHTML);
-      dictionary.updateWordCount();
-      view.assignWords();
-      view.updateScore();
-      view.resetTimer();
-      view.displayTimer();
-    } else {
-      if(!view.gameOver) {
-        view.endGame(event.target);
-      }
+  var getCorrectChoice = function(word) {
+    var output;
+
+    do {
+      output = word.shuffle();
+    } while(output === word);
+
+    return output;
+  };
+
+  /**
+   * Returns an incorrect answer for the current word
+   */
+  var getIncorrectChoice = function(word) {
+    var output;
+
+    do {
+      output = word.replaceLetter();
+    } while(output.isCorrect());
+
+    return output.shuffle();
+  };
+
+  // Game object used to to manipulate the timer, score, button choices, current word, and current level
+  var game = {};
+
+  // Cacheing elements
+  game.currentWord = document.getElementById('currentWord');
+  game.timer = document.getElementById('timer');
+  game.score = document.getElementById('score');
+  game.buttons = document.getElementsByClassName('anagramButton');
+  game.lossScreen = document.getElementById('loss_screen');
+  game.finalScore = document.getElementById('final_score');
+  game.resetButton = document.getElementById('reset');
+
+  // Reference to the current button with the correct answer
+  game.correctButton = null;
+
+  // Current difficulty of the game
+  game.currentGlossary = null;
+
+  // Level number used to choose the appropriate glossary
+  game.level = 0;
+
+  // Score number used to keep track of the player's current score
+  game.scoreNum = 0;
+
+  // The remaining time left for the countdown timer
+  game.secondsLeft = 500;
+
+  // The Glossaries for each of the difficulty levels
+  game.glossaries = {
+    veryEasy: new Glossary([
+      'act', 'bar', 'car', 'dew', 'eat', 'far', 'gym',
+      'hey', 'ink', 'jet', 'key', 'log', 'mad', 'nap',
+      'odd', 'pal', 'ram', 'saw', 'tan', 'urn', 'vet',
+      'wed', 'zoo', 'the', 'and', 'for', 'are', 'but',
+      'not', 'you', 'all', 'any', 'can', 'her', 'was',
+      'one', 'our', 'out', 'day', 'get', 'has', 'him',
+      'his', 'how', 'man', 'new', 'now', 'old', 'see'
+    ]),
+    easy: new Glossary([
+      'that', 'with', 'have', 'this', 'will', 'your', 'from',
+      'they', 'know', 'want', 'been', 'good', 'much', 'some',
+      'time', 'very', 'when', 'come', 'here', 'just', 'like',
+      'long', 'make', 'many', 'more', 'only', 'over', 'such',
+      'take', 'than', 'them', 'well', 'were', 'aeon', 'aero',
+      'back', 'bang', 'bell', 'bios', 'blog', 'blot', 'bots',
+      'cafe', 'edgy', 'navy', 'fold', 'spew', 'tact', 'howl'
+    ]),
+    medium:  new Glossary([
+      'adept', 'aphid', 'aphis', 'apoda', 'apsis', 'arena', 'argon',
+      'argus', 'aroph', 'aster', 'augur', 'aurin', 'baric', 'bathe',
+      'being', 'beroe', 'chair', 'chart', 'cheer', 'cilia', 'cloud',
+      'death', 'delph', 'devil', 'evade', 'exode', 'exult', 'farse',
+      'fever', 'flesh', 'flock', 'hepar', 'hexad', 'hocco', 'homer',
+      'houri', 'lumen', 'lymph', 'phono', 'photo', 'rheic', 'rhein',
+      'rogue', 'romic', 'rosin', 'rouge', 'scrat', 'screw', 'sense'
+    ]),
+    hard: new Glossary([
+      'dossel', 'dosser', 'dosses', 'dossil', 'dotage', 'dotard', 'doters',
+      'formee', 'former', 'formes', 'formic', 'formol', 'formyl', 'fornix',
+      'ibises', 'icebox', 'icecap', 'iceman', 'icemen', 'ichors', 'icicle',
+      'invite', 'invoke', 'inwall', 'inward', 'inwind', 'inwove', 'inwrap',
+      'purvey', 'pushed', 'pusher', 'pushes', 'pushup', 'pusley', 'pusses',
+      'regret', 'regrew', 'regrow', 'reguli', 'rehabs', 'rehang', 'rehash',
+      'sorned', 'sorner', 'sorrel', 'sorrow', 'sorted', 'sorter', 'sortie'
+    ]),
+    insane: new Glossary([
+      'aeriest', 'aerobat', 'aerobes', 'aerobia', 'aerobic', 'aerogel',
+      'aerosat', 'aerosol', 'aerugos', 'chortle', 'choughs', 'choused',
+      'chouser', 'chouses', 'chowder', 'chowing', 'chowsed', 'chowses',
+      'creaked', 'creamed', 'creamer', 'creased', 'creaser', 'creases',
+      'created', 'creates', 'creatin', 'diffuse', 'digamma', 'digests',
+      'diggers', 'digging', 'dighted', 'digital', 'diglots', 'dignify',
+      'echoism', 'eclairs', 'eclipse', 'eclogue', 'ecocide', 'ecology',
+      'economy', 'ecotage', 'ecotone', 'faintly', 'fairest', 'fairies',
+      'fairing', 'fairish', 'fairway', 'faithed', 'faitour', 'fajitas',
+      'idiotic', 'idlesse', 'idolise', 'idolism', 'idolize', 'idylist'
+    ]),
+    suicidal: new Glossary([
+      'agatizes', 'agedness', 'agemates', 'agencies', 'agendums', 'ageneses',
+      'agenesia', 'agenesis', 'backwash', 'backwood', 'backwrap', 'backyard',
+      'baclofen', 'bacteria', 'bacterin', 'baculine', 'blasters', 'blastier',
+      'blasties', 'blasting', 'blastoff', 'blastoma', 'blastula', 'blatancy',
+      'cochleas', 'cocinera', 'cockaded', 'cockades', 'cockapoo', 'cockatoo',
+      'cockbill', 'cockboat', 'exorcise', 'exorcism', 'exorcist', 'exorcize',
+      'exordial', 'exordium', 'exosmose', 'exospore', 'headfish', 'headfuls',
+      'headgate', 'headgear', 'headhunt', 'headiest', 'headings', 'headlamp',
+      'smuggled', 'smuggler', 'smuggles', 'smugness', 'smushing', 'smutched'
+    ])
+  };
+
+  /**
+   * Updates the current glossary being used based on your current level
+   */
+  game.updateGlossary = function() {
+    // Increase the difficulty after every 10 points earned by the player
+    if(this.scoreNum !== 0 && this.scoreNum % 10 === 0) {
+      alert('you have leveled up!');
+      this.levelUp();
+    }
+
+    var glossaries = this.glossaries;
+
+    // Choose the glossary based on the current level
+    switch(this.level) {
+      case 0:
+        this.currentGlossary = glossaries.veryEasy;
+        break;
+      case 1:
+        this.currentGlossary = glossaries.easy;
+        break;
+      case 2:
+        this.currentGlossary = glossaries.medium;
+        break;
+      case 3:
+        this.currentGlossary = glossaries.hard;
+        break;
+      case 4:
+        this.currentGlossary = glossaries.insane;
+        break;
+      default:
+        this.currentGlossary = glossaries.suicidal;
     }
   };
 
   /**
-   * Randomly assigns the current word and correct and incorrect choices to the four buttons
+   * Removes the previous word used and updates the remaining word count of the glossary
    */
-  view.assignWords = function() {
-    // Randomly choose a word to use as the anagram question
-    this.currWord.innerHTML = dictionary.randWord();
-    var word = view.currWord.innerHTML;
+  game.removePrevWord = function() {
+    var prevWord = this.currentWord.innerHTML;
+
+    // Remove the previous word from the current glossary
+    this.currentGlossary.removeWord(prevWord);
+  };
+
+  /**
+   * Updates the current word with a random word from the current glossary
+   */
+  game.updateWord = function() {
+    // Remove the previous word that was used from the current glossary
+    this.removePrevWord();
+
+    // Update the current word to a random word from the current glossary
+    this.currentWord.innerHTML = this.currentGlossary.randWord();
+  };
+
+  /**
+   * Assigns the correct and incorrect choices to each button
+   */
+  game.updateButtons = function() {
+    // Index used with for-loops
+    var i;
 
     // Choose a random button to hold the correct choice
-    dictionary.correctButton = this.buttons[Math.floor(Math.random() * 4)];
-    dictionary.correctButton.innerHTML = dictionary.getCorrectChoice(word);
+    this.correctButton = this.buttons[Math.floor(Math.random() * 4)];
+    this.correctButton.innerHTML = getCorrectChoice(this.currentWord.innerHTML);
 
     // Give the rest of the buttons incorrect choices
-    for(var i = 0; i < this.buttons.length; i++) {
-      if(this.buttons[i] === dictionary.correctButton) { continue; }
+    for(i = 0; i < this.buttons.length; i++) {
+      if(this.buttons[i] === this.correctButton) { continue; }
+      this.buttons[i].innerHTML = getIncorrectChoice(this.currentWord.innerHTML);
+    }
 
-      // Assign each button an incorrect choice and make sure it isn't actually correct
-      this.buttons[i].innerHTML = dictionary.getIncorrectChoice(word);
-      while(dictionary.isCorrect(this.buttons[i].innerHTML)) {
-        this.buttons[i].innerHTML = dictionary.getIncorrectChoice(word);
-      }
+    // Add event handlers to each button
+    for(i = 0; i < this.buttons.length; i++) {
+      this.buttons[i].addEventListener('click', this.clickHandler);
     }
   };
 
   /**
-   * Displays and updates the timer
+   * Starts the countdown timer
    */
-  view.displayTimer = function() {
+  game.displayTimer = function() {
     // Clear the previous timer
-    clearInterval(view.timer);
-    view.timer = setInterval(function() {
-      if(view.seconds === 0) {
-        // If the timer reaches zero, clear the timer and display a loss screen
-        clearInterval(view.timer);
-        view.endGame();
+    clearInterval(game.timer);
+    game.timer = setInterval(function() {
+      if(game.secondsLeft === 0) {
+        // If the timer reaches zero, clear the timer and end the game
+        clearInterval(game.timer);
+        game.over();
       } else {
-        view.seconds--;
+        game.secondsLeft--;
       }
       var time = document.getElementById('timer');
-      time.innerHTML = (view.seconds / 100).toFixed(2);
+      time.innerHTML = (game.secondsLeft / 100).toFixed(2);
     }, 10);
   };
 
   /**
-   * Resets the timer back to the beginning
+   * Resets the countdown timer
    */
-  view.resetTimer = function() {
-    view.seconds = 500;
+  game.resetTimer = function() {
+    game.secondsLeft = 500;
   };
 
   /**
-   * Updates the player's current score
+   * Handler for pressing the correct and incorrect buttons
    */
-  view.updateScore = function() {
+  game.clickHandler = function(event) {
+    var clickedButton = event.target;
+    if(clickedButton === game.correctButton) {
+      game.resetTimer();
+      game.updateScore();
+      game.removePrevWord();
+      game.render();
+    } else {
+      game.over(clickedButton);
+    }
+  };
+
+  /**
+   * Increments the current level by 1
+   */
+  game.levelUp = function() {
+    this.level++;
+  };
+
+  /**
+   * Increments the current score by 1
+   */
+  game.updateScore = function() {
     this.scoreNum++;
-    this.score.innerHTML = 'Score: ' + this.scoreNum;
     if(this.scoreNum.toString().length < 2) {
-      this.score.innerHTML = 'Score: 0' + this.scoreNum;
+      this.scoreNum = '0' + this.scoreNum;
     }
-  };
-
-  /**
-   * Stops the timer, shows the correct answer, and ends the game
-   */
-  view.endGame = function(clickedButton) {
-    if(clickedButton && !this.gameOver) {
-      // Show their incorrect choice in red (only if a wrong choice was made)
-      clickedButton.style.background = '#ff3333';
-      clickedButton.style.borderWidth = '5px';
-      clickedButton.style.borderColor = 'red';
-    }
-
-    // Show the correct answer in green
-    dictionary.correctButton.style.background = 'green';
-    dictionary.correctButton.style.borderWidth = '5px';
-
-    // Stop the timer
-    view.seconds = 0;
-
-    // Set the status of the game to be game over
-    this.gameOver = true;
-
-    // Go to the loss screen after a couple of seconds
-    setTimeout(view.displayLossScreen, 1500);
+    this.score.innerHTML = 'Score: ' + this.scoreNum;
   };
 
   /**
    * Displays the loss screen
    */
-  view.displayLossScreen = function() {
-    // Reveal the loss screen and show the final score
-    document.getElementById('lossScreen').style.zIndex = '10';
-    document.getElementById('lossScreen').style.opacity = '1';
-    document.getElementById('finalScore').innerHTML = view.scoreNum;
+  game.displayLossScreen = function() {
+    // Display the loss screen
+    game.lossScreen.style.opacity = '1';
+    game.lossScreen.style.zIndex = '10';
 
-    // Create the reset button
-    var resetButton = document.getElementById('reset');
-    resetButton.addEventListener('click', function() {
+    // Display the final score
+    game.finalScore.innerHTML = game.scoreNum;
+
+    // Add event handler to the reset button
+    game.resetButton.addEventListener('click', function() {
       window.location = 'anagrams.html';
     });
   };
 
   /**
-   * Starts the game by assigning the current word, the button choices, and displaying the timer
+   * Ends the game
    */
-  view.render = function() {
-    view.assignWords();
-    view.displayTimer();
+  game.over = function(clickedButton) {
+    // Stop the timer
+    game.secondsLeft = 0;
 
+    // Disable all of the buttons by removing their event handlers
     for(var i = 0; i < this.buttons.length; i++) {
-      this.buttons[i].addEventListener('click', view.makeClickHandler);
+      this.buttons[i].removeEventListener('click', this.clickHandler);
     }
+
+    // If a wrong choice was made, show their incorrect choice in red
+    if(clickedButton) {
+      clickedButton.style.background = 'red';
+      clickedButton.style.border = 'red';
+    }
+
+    // Show the correct answer in green
+    this.correctButton.style.background = 'green';
+    this.correctButton.style.borderWidth = '2px';
+
+    // Display the loss screen
+    setTimeout(game.displayLossScreen, 1500);
   };
 
-  view.render();
+  /**
+   * Renders the current word and button choices
+   */
+  game.render = function() {
+    this.updateGlossary();
+    this.updateWord();
+    this.updateButtons();
+    this.displayTimer();
+  };
+
+  game.render();
 })();
